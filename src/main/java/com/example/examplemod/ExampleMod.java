9738d52f67e32b108084d6f30e8241967a6416e3
Ryan Michela <rmichela@salesforce.com>
2019-10-13 14:58:38 -0400

    First event handler

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,22 +1,13 @@
 package com.example.examplemod;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("stormhammer")
@@ -28,5 +19,16 @@ public class ExampleMod
     public ExampleMod() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onHurt(LivingHurtEvent event) {
+        Entity attacker = event.getSource().getImmediateSource();
+        Entity injured = event.getEntity();
+        DamageSource injury = event.getSource();
+
+        LOGGER.info(">>> " + attacker.getName().getString() +
+                " hurt " + injured.getName().getString() +
+                " with " + injury.getDamageType());
     }
 }
